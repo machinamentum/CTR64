6f45e130d60c9fbd5d12b35114e594a245294b7c
Josh Huelsman <joshuahuelsman@gmail.com>
2015-08-15 19:08:33 -0400

    Restructured BIOS layout.

--- bios/source/start.s
+++ bios/source/start.s
@@ -1,12 +1,15 @@
 #include <asm/regdef.h>
-    .text
+    .section .text.boot
     .global _start
 _start:
-    nop
-    nop
-    sw $0, 0x80
-    nop
-    nop
-    b _start
-    .rdata
+    # TODO start up code
+    b _kernel_proper
+
+    .section .rodata.rom_header
+    .int KERNEL_BUILD_DATE
+    .int 0x43545258
     .asciz "CTRX BIOS"
+
+    .section .text
+_kernel_proper:
+    b _start
