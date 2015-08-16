c2d6e7bbf9eee9d841dc15f71233ccbbed653dfe
Josh Huelsman <joshuahuelsman@gmail.com>
2015-08-15 21:41:39 -0400

    Updated test BIOS

--- bios/source/start.s
+++ bios/source/start.s
@@ -1,10 +1,14 @@
 #include <asm/regdef.h>
+    .set		noreorder
     .section .text.boot
     .global _start
 _start:
+    nop
+    nop
+    nop
     # TODO start up code
     b _kernel_proper
-
+    nop
     .section .rodata.rom_header
     .int KERNEL_BUILD_DATE
     .int 0x43545258
