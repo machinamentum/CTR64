36946bfdcc03e56cea738478420c3da02105469b
Josh Huelsman <joshuahuelsman@gmail.com>
2015-08-20 22:22:58 -0400

    Kernel now draws a quad using the GPU.

--- bios/source/kernel.c
+++ bios/source/kernel.c
@@ -2,7 +2,7 @@
 #include <stdint.h>
 
 #define GPCOM(func, param) \
-(((func & 0xFF) << 24) | (param & 0x00FFFFFF))
+(((func & 0xFF) << 24) | ((param) & 0x00FFFFFF))
 
 uint32_t * const GP0 = (uint32_t *)0x1F801810;
 uint32_t * const GP1 = (uint32_t *)0x1F801814;
@@ -27,14 +27,38 @@ static inline void enable_display()
     // turn on display
     write_gp1(GPCOM(0x03, 0));
 
-    // fill rect red
-    write_gp0(GPCOM(0x02, 0x0000ff));
+    // fill rect black
+    write_gp0(GPCOM(0x02, 0x000000));
     write_gp0(0); // xy
     write_gp0((240 << 16) | (320));//width, height
+
+    //enable drawing to display
+    write_gp0(GPCOM(0xE1, 1 << 10));
+
+    //set drawing area
+    write_gp0(GPCOM(0xE3, 0));
+    write_gp0(GPCOM(0xE4, 240 | (320 << 10)));
+}
+
+const static uint16_t Quad[8] = {
+    200, 200,
+    0, 200,
+    200, 0,
+    0, 0
+};
+
+static void DrawQuad(const uint16_t *Data)
+{
+    write_gp0(GPCOM(0x28, 0xFFFFFF));
+    write_gp0(Data[0] | (Data[1] << 16));
+    write_gp0(Data[2] | (Data[3] << 16));
+    write_gp0(Data[4] | (Data[5] << 16));
+    write_gp0(Data[6] | (Data[7] << 16));
 }
 
 void kmain(void)
 {
     enable_display();
+    DrawQuad(Quad);
     while (1){}
 }
