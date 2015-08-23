222f01e6898d6f06cde80e354e753ba71f489199
Josh Huelsman <joshuahuelsman@gmail.com>
2015-08-23 07:59:40 -0400

    Added license to source files.

--- source/disasm.cpp
+++ source/disasm.cpp
@@ -1,4 +1,11 @@
-
+/*
+ * ----------------------------------------------------------------------------
+ * "THE BEER-WARE LICENSE" (Revision 94):
+ * <joshuahuelsman@gmail.com> wrote this file. As long as you retain this notice you
+ * can do whatever you want with this stuff. If we meet some day, and you think
+ * this stuff is worth it, you can buy me a beer in return.   Josh Huelsman
+ * ----------------------------------------------------------------------------
+ */
 #include <3ds.h>
 #include <cstdio>
 #include <cstdlib>
