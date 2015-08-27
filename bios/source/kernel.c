a93d097327e889bacf22425b1fe8e4f0bccebe51
Josh Huelsman <joshuahuelsman@gmail.com>
2015-08-27 00:32:31 -0400

    Clean up kernel code.

--- bios/source/kernel.c
+++ bios/source/kernel.c
@@ -16,56 +16,60 @@ uint32_t * const GP1 = (uint32_t *)0x1F801814;
 static const uint32_t *GPUREAD = (uint32_t *)0x1F801810;
 static const uint32_t *GPUSTAT = (uint32_t *)0x1F801814;
 
-static inline void write_gp0(uint32_t command)
+static inline void
+WriteGP0(uint32_t command)
 {
     *GP0 = command;
 }
 
-static inline void write_gp1(uint32_t command)
+static inline void
+WriteGP1(uint32_t command)
 {
     *GP1 = command;
 }
 
-static inline void enable_display()
+static inline void
+EnableDisplay()
 {
     // Reset GPU
-    write_gp1(0);
+    WriteGP1(0);
 
     // turn on display
-    write_gp1(GPCOM(0x03, 0));
+    WriteGP1(GPCOM(0x03, 0));
 
     // fill rect black
-    write_gp0(GPCOM(0x02, 0x000000));
-    write_gp0(0); // xy
-    write_gp0((240 << 16) | (320));//width, height
+    WriteGP0(GPCOM(0x02, 0x000000));
+    WriteGP0(0); // xy
+    WriteGP0((240 << 16) | (320));//width, height
 
     //enable drawing to display
-    write_gp0(GPCOM(0xE1, 1 << 10));
+    WriteGP0(GPCOM(0xE1, 1 << 10));
 
     //set drawing area
-    write_gp0(GPCOM(0xE3, 0));
-    write_gp0(GPCOM(0xE4, 240 | (320 << 10)));
+    WriteGP0(GPCOM(0xE3, 0));
+    WriteGP0(GPCOM(0xE4, 240 | (320 << 10)));
 }
 
 const static uint16_t Quad[8] = {
-    200, 200,
-    0, 200,
-    200, 0,
+    320, 240,
+    0, 240,
+    320, 0,
     0, 0
 };
 
-static void DrawQuad(const uint16_t *Data)
+static void
+DrawQuad(const uint16_t *Data)
 {
-    write_gp0(GPCOM(0x28, 0xFFFFFF));
-    write_gp0(Data[0] | (Data[1] << 16));
-    write_gp0(Data[2] | (Data[3] << 16));
-    write_gp0(Data[4] | (Data[5] << 16));
-    write_gp0(Data[6] | (Data[7] << 16));
+    WriteGP0(GPCOM(0x28, 0xDB7093));
+    WriteGP0(Data[0] | (Data[1] << 16));
+    WriteGP0(Data[2] | (Data[3] << 16));
+    WriteGP0(Data[4] | (Data[5] << 16));
+    WriteGP0(Data[6] | (Data[7] << 16));
 }
 
 void kmain(void)
 {
-    enable_display();
+    EnableDisplay();
     DrawQuad(Quad);
     while (1){}
 }
