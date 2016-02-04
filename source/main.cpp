5847aaf85fe9c8fed0520f96caea049ed4d25ea6
Josh Huelsman <joshuahuelsman@gmail.com>
2016-02-03 20:39:43 -0500

    Clean up main

--- source/main.cpp
+++ source/main.cpp
@@ -11,11 +11,8 @@
 #include <stdlib.h>
 #include <cstring>
 #include "mips.h"
-#include "gpu.h"
 #include "disasm.h"
-#include "psxexe.h"
 #include "joypad.h"
-#include "hsf.h"
 
 void
 ResetCpu(MIPS_R3000 *Cpu)
@@ -41,142 +38,6 @@ empty_write(void *Obj, u32 Address)
 
 }
 
-static hsf HSF;
-static hsf_file *FilePtrs[15];
-static int TakenFiles[15] = {};
-static int ReturnFile = 0;
-static int ReturnRead = 0;
-static int ReturnSeek = 0;
-static int ReturnFirstFile = 0;
-
-static void
-CTRXFileOpen(void *Ref, u32 Ptr)
-{
-    char *FileName = (char *)MapVirtualAddress((MIPS_R3000 *)Ref, Ptr);
-    int FreeFile = -1;
-    for (int i = 2; i < 15; ++i)
-    {
-        if (TakenFiles[i] == 0)
-        {
-            FreeFile = i;
-        }
-    }
-
-    if (FreeFile > 1)
-    {
-        printf("CTRX: Opening file %s\n", FileName);
-        FilePtrs[FreeFile] = HsfFileOpen(&HSF, FileName);
-        if (!FilePtrs[FreeFile]->DirectoryEntry)
-        {
-            printf("Error opening file: %s\n", FileName);
-            ReturnFile = -1;
-            return;
-        }
-
-        TakenFiles[FreeFile] = 1;
-    }
-
-    ReturnFile = FreeFile;
-}
-
-static u32
-CTRXFileOpenReturn(void *Ref, u32 Address)
-{
-    return ReturnFile;
-}
-
-static void
-CTRXFileRead(void *Ref, u32 Ptr)
-{
-    MIPS_R3000 *Cpu = (MIPS_R3000 *)Ref;
-    struct FReadInfo
-    {
-        int Fd;
-        int Dst;
-        int Length;
-    } *FReadInfoPtr;
-    FReadInfoPtr = (FReadInfo *)MapVirtualAddress(Cpu, Ptr);
-    ReturnRead = FReadInfoPtr->Length;
-    HsfFileRead(MapVirtualAddress(Cpu, FReadInfoPtr->Dst), 1, FReadInfoPtr->Length, FilePtrs[FReadInfoPtr->Fd]);
-}
-
-static u32
-CTRXFileReadReturn(void *Ref, u32 Address)
-{
-    return ReturnRead;
-}
-
-static void
-CTRXFileSeek(void *Ref, u32 Ptr)
-{
-    MIPS_R3000 *Cpu = (MIPS_R3000 *)Ref;
-    struct FSeekInfo
-    {
-        int Fd;
-        int Offset;
-        int SeekType;
-    } *FSeekInfoPtr;
-    FSeekInfoPtr = (FSeekInfo *)MapVirtualAddress(Cpu, Ptr);
-    HsfFileSeek(FilePtrs[FSeekInfoPtr->Fd], FSeekInfoPtr->Offset, FSeekInfoPtr->SeekType);
-    ReturnSeek = HsfFileTell(FilePtrs[FSeekInfoPtr->Fd]);
-}
-
-static u32
-CTRXFileSeekReturn(void *Ref, u32 Address)
-{
-    return ReturnSeek;
-}
-
-static void
-CTRXFirstFile(void *Ref, u32 Ptr)
-{
-    MIPS_R3000 *Cpu = (MIPS_R3000 *)Ref;
-
-    struct DirEntry
-    {
-        char FileName[0x14];
-        int Attribute;
-        int Size;
-        void *Next;
-        int SelectorNumber;
-        int Reserved;
-    };
-
-    struct FFInfo
-    {
-        char *FileName;
-        u32 Entry;
-    } *FFInfoPtr;
-
-    FFInfoPtr = (FFInfo *)MapVirtualAddress(Cpu, Ptr);
-    DirEntry *Entry = (DirEntry *)MapVirtualAddress(Cpu, FFInfoPtr->Entry);
-    snprintf(Entry->FileName, 0x14, "%s", (char *)MapVirtualAddress(Cpu, (u32)FFInfoPtr->FileName));
-
-    printf("CTRX filefile: %s\n", Entry->FileName);
-
-    auto GetFileSize = [](const char *FileName)
-    {
-        hsf_file *f = HsfFileOpen(&HSF, FileName);
-        if (!f)
-        {
-            printf("Could not open file: %s\n", FileName);
-            return 0L;
-        }
-        HsfFileSeek(f, 0, SEEK_END);
-        long fsize = HsfFileTell(f);
-        HsfFileSeek(f, 0, SEEK_SET);
-        HsfFileClose(f);
-        return fsize;
-    };
-    Entry->Size = GetFileSize(Entry->FileName);
-    ReturnFirstFile = (int)FFInfoPtr->Entry;
-}
-
-static u32
-CTRXFirstFileReturn(void *Ref, u32 Address)
-{
-    return ReturnFirstFile;
-}
 
 static u32 InterruptMask;
 
@@ -196,24 +57,9 @@ int main(int argc, char **argv)
 {
     InitPlatform(argc, argv);
 
-    HsfOpen(&HSF, "puzzle.hsf");
-
     MIPS_R3000 Cpu;
-    GPU Gpu;
-    Cpu.CP1 = &Gpu;
-    MapRegister(&Cpu, (mmr) {GPU_GP0, &Gpu, GpuGp0, empty_ret});
-    MapRegister(&Cpu, (mmr) {GPU_GP1, &Gpu, GpuGp1, GpuStat});
-    MapRegister(&Cpu, (mmr) {0x1F802064, &Cpu, std_out_putchar, empty_ret});
-    MapRegister(&Cpu, (mmr) {0x1F802068, &Cpu, CTRXFileOpen, CTRXFileOpenReturn});
-    MapRegister(&Cpu, (mmr) {0x1F802070, &Cpu, CTRXFileRead, CTRXFileReadReturn});
-    MapRegister(&Cpu, (mmr) {0x1F802074, &Cpu, CTRXFileSeek, CTRXFileSeekReturn});
-    MapRegister(&Cpu, (mmr) {0x1F802078, &Cpu, CTRXFirstFile, CTRXFirstFileReturn});
-    MapRegister(&Cpu, (mmr) {0x1F8010A0, &Cpu, DMA2Trigger, empty_ret});
-    MapRegister(&Cpu, (mmr) {JOY_TX_DATA, nullptr, JoyTxWrite, JoyRxRead});
-    MapRegister(&Cpu, (mmr) {0x1F801070, nullptr, empty_write, CTRXInterruptRegisterRead});
-    MapRegister(&Cpu, (mmr) {0x1F801070, nullptr, CTRXInterruptRegisterWrite, empty_ret});
 
-    FILE *f = fopen("psx_bios.bin", "rb");
+    FILE *f = fopen("n64_ipl.bin", "rb");
     fseek(f, 0, SEEK_END);
     long fsize = ftell(f);
     fseek(f, 0, SEEK_SET);
@@ -264,8 +110,6 @@ int main(int argc, char **argv)
 
         SwapBuffersPlatform();
     }
-
-    HsfClose(&HSF);
     ExitPlatform();
 
     return 0;
